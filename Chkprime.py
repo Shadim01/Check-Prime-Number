ace46a4b91cef18f90372176600698092b144d0b
Al Samhun Shadim <57861612+Shadim01@users.noreply.github.com>
2020-07-23 19:46:17 +0300

    Update Chkprime.py

--- Chkprime.py
+++ Chkprime.py
@@ -1,11 +1,11 @@
 #Check Prime Number
-user_input = int(input("Number:"))
+number = int(input("Number:"))
 
-if user_input > 1:
-	for i in range(2, user_input):
-		if (user_input % i) == 0:
-			print(f" {user_input}, is not a prime number")
-			print(f"{i} times {user_input//i} is {user_input}")
+if number > 1:
+	for i in range(2, number):
+		if (number % i) == 0:
+			print(f" {number}, is not a prime number")
+			print(f"{i} times {number//i} is {number}")
 			break
 	else:
-			print(f"{user_input}, This is prime")
+			print(f"{number}, This is prime")
